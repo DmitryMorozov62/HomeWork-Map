28860142f33f284833a57b52e7768dfab107fd57
admin <62morozov.dima.90@gmail.com>
2023-04-02 07:44:42 +0300

    Initial commit

---
+++ src/main/java/HomeWork/HomeWorkListApplication.java
@@ -0,0 +1,13 @@
+package HomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkListApplication.class, args);
+	}
+
+}
